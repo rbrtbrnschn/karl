3aed1a3119fffca892b49ff7173e3b29d51c5b57
Robert Bornschein <robertbornscheinofficial@gmail.com>
2023-01-07 12:16:54 +0100

    chore: generate nest app

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,8 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useEffect } from 'react';
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  useEffect(()=>{
+    console.log("Started up")
+    fetch("https://www.internate.org/toechter-und-soehne-blog/wp/v2/posts",{mode: 'cors'})
+      .then((res)=>res.json())
+      .then(console.log);
+  },[])
   return (
     <>
       <NxWelcome title="client" />
