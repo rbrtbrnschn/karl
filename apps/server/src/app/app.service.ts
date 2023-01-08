3dabf4fef13318cef07e567d2fccf281a82ea733
Robert Bornschein <robertbornscheinofficial@gmail.com>
2023-01-08 18:40:55 +0100

    refactor: string parsing logic

--- apps/server/src/app/app.service.ts
+++ apps/server/src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import fetch from "node-fetch";
-import {parseDocument} from 'htmlparser2';
+import {parseString, stringToCountMap, stripHTMLEntities} from "@karl/common";
 
 @Injectable()
 export class AppService {
@@ -14,50 +14,14 @@ export class AppService {
     const suffix = "/wp-json/wp/v2/posts";
     const URI = `${IS_TESTING ? testBase : base}${suffix}`;
 
-    function handleCountMap(string:string){
-      return string.split(" ").filter(e=>e!=="").reduce((acc,curr)=>{
-        acc[curr]=(acc[curr]||0)+1;
-        return acc;
-      },{})
-    }
-
-    function parseString(string:string){
-      const REG = /(<([^>]+)>)/ig;
-      const strippedTags= string.replace(REG, '');
-      const strippedHTMLEntities = stripHTMLEntities(strippedTags);
-      const strippedCharacters = strippedHTMLEntities.replace(/[.,;:!?()[\]{}"“\\$”/*&\-\–\s]/g,()=>{
-        // TODO refactor to allow urls to stay untouched
-        return " ";
-      });
-      const strippedLowered = strippedCharacters.toLowerCase();
-
-      return strippedLowered;
-    }
-
-    /**
-      * Credit to https://stackoverflow.com/questions/44195322/a-plain-javascript-way-to-decode-html-entities-works-on-both-browsers-and-node
-      */
-    function stripHTMLEntities(encodedString: string) {
-    var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
-    var translate = {
-        "nbsp":" ",
-        "amp" : "&",
-        "quot": "\"",
-        "lt"  : "<",
-        "gt"  : ">"
-    };
-    return encodedString.replace(translate_re, function(match, entity) {
-        return translate[entity];
-    }).replace(/&#(\d+);/gi, () => "");
-}
-
-    return fetch(URI)
+    //@TODO create new service and refactor this logic to there, and call from service on interval
+      return fetch(URI)
       .then(response => response.json())
       .then(json => {
         const tbd = Array.from(json).map((e:any)=>{
           const rendered = e?.content?.rendered || "";
           const parsed = parseString(rendered);
-          const res = handleCountMap(parsed);
+          const res = stringToCountMap(parsed);
           console.log(res);
           return res;
         })
