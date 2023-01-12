42b7deb28d59a4aed86afa8b365c937d2754709e
Robert Bornschein <robertbornscheinofficial@gmail.com>
2023-01-12 14:06:27 +0100

    feat-client: alert component

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,74 +1,80 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { useEffect, useState } from 'react'
-import styles from './app.module.scss'
-import { io, Socket } from 'socket.io-client'
-import { IGatewayResponse, parseString } from '@karl/common'
-import { Card } from './components/card/card'
+import { useEffect, useState } from 'react';
+import styles from './app.module.scss';
+import { io, Socket } from 'socket.io-client';
+import { IGatewayResponse, parseString } from '@karl/common';
+import { Card } from './components/card/card';
+import { Alert } from './components/alert/alert';
 
 interface ServerToClientEvents {
-  noArg: () => void
-  basicEmit: (a: number, b: string, c: Buffer) => void
-  withAck: (d: string, callback: (e: number) => void) => void
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
 }
 
 interface ClientToServerEvents {
-  events: (res?: any) => void
+  events: (res?: any) => void;
 }
 
 interface InterServerEvents {
-  ping: () => void
+  ping: () => void;
 }
 
 interface SocketData {
-  name: string
-  age: number
+  name: string;
+  age: number;
 }
 
 export function App() {
-  const [state, setState] = useState<IGatewayResponse[]>([])
-  const [isConnected, setIsConnected] = useState(false)
+  const [state, setState] = useState<IGatewayResponse[]>([]);
+  const [isConnected, setIsConnected] = useState(false);
   useEffect(() => {
     const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
       `http://localhost:${process.env.WS_PORT || 8001}`
-    )
+    );
     socket.on('connect', () => {
-      setIsConnected(true)
-    })
+      setIsConnected(true);
+    });
     socket.on('disconnect', () => {
-      setIsConnected(false)
-    })
+      setIsConnected(false);
+    });
 
     //@TODO fix typing
     //@ts-ignore
     socket.on('init', (e) => {
-      setState(e)
-      console.log('basic emit', e)
-    })
-  }, [])
+      setState(e);
+      console.log('Received Data:', e);
+    });
+  }, []);
   return (
     <>
       <div className={'container mx-auto'}>
+        <Alert
+          title="Using different URL for testing."
+          description=" Default URL: https://animallogic.com"
+          className="mt-2"
+        />
         {state.map((entry, index) => {
           const List = Object.entries(entry.countmap).map(
             ([word, amount]: any) => {
               return (
                 <tr
-                  className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                   key={word + amount}
                 >
                   <td
-                    scope='row'
-                    className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
                     {word}
                   </td>
-                  <td scope='row' className='px-6 py-4'>
+                  <td scope="row" className="px-6 py-4">
                     {amount}
                   </td>
                 </tr>
-              )
+              );
             }
-          )
+          );
           return (
             <div key={entry.id}>
               <Card
@@ -83,14 +89,14 @@ export function App() {
                 likes={1}
                 comments={4}
               />
-              <div className='relative overflow-x-auto'>
-                <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-                  <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+              <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                      <th scope='col' className='px-6 py-3'>
+                      <th scope="col" className="px-6 py-3">
                         Word
                       </th>
-                      <th scope='col' className='px-6 py-3'>
+                      <th scope="col" className="px-6 py-3">
                         Amount
                       </th>
                     </tr>
@@ -99,11 +105,11 @@ export function App() {
                 </table>
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- todo.md
+++ todo.md
@@ -28,7 +28,7 @@
 - TODO:
 - setup docker V
 - fix frontend tests V
-- showcase disclaimer saying couldnt use original url
+- showcase disclaimer saying couldnt use original url V
 - showcase loading in frontend or hero
 - refactor frontend components
 - remove vercel  from github repo 
