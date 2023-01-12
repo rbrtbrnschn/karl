df2fa5f1cf2c2ffd028cffecc7c3006e2f51fe41
Robert Bornschein <robertbornscheinofficial@gmail.com>
2023-01-12 15:40:44 +0100

    feat: swagger descriptions

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
@@ -7,6 +8,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Boilerplate Route.',
+  })
   getData() {
     return this.appService.getData();
   }
--- apps/server/src/countmap/countmap.controller.ts
+++ apps/server/src/countmap/countmap.controller.ts
@@ -1,21 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { CountmapService } from './countmap.service';
 
 @Controller('countmap')
 export class CountmapController {
-  constructor(private readonly countmapService: CountmapService){}
+  constructor(private readonly countmapService: CountmapService) {}
 
+  @ApiResponse({
+    status: 200,
+    description: 'Returns JSON countmap used in http://localhost:3000',
+  })
   @Get()
-  getData(){
+  getData() {
     const IS_TESTING = true;
 
-    const base = "https://www.internate.org/";
-    const testBases = ["https://animallogic.com","https://www.angrybirds.com"];
+    const base = 'https://www.internate.org/';
+    const testBases = ['https://animallogic.com', 'https://www.angrybirds.com'];
     const testBase = testBases[0];
 
-    const suffix = "/wp-json/wp/v2/posts";
+    const suffix = '/wp-json/wp/v2/posts';
     const URI = `${IS_TESTING ? testBase : base}${suffix}`;
 
-    return this.countmapService.createFromUrl(URI)
+    return this.countmapService.createFromUrl(URI);
   }
 }
